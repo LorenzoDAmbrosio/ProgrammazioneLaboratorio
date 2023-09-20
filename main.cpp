b0e8f99192c0c8f60e6fd250e86d9238ea3d4350
Lorenzo D'Ambrosio <lorenzoda2001@gmail.com>
2023-09-20 15:05:53 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+/*
+ * 2023 09 20
+ * Creato da Lorenzo D'Ambrosio 7050275
+ *
+ *
+ *
+ *
+ *
+ * */
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
